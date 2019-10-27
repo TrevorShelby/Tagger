1e885a9f545c045ddf664372daf63e87bed2fb72
TrevorShelby <trvrshelby@gmail.com>
2019-10-27 15:33:27 -0500

    Renamed variables in add

--- cmd/add.js
+++ cmd/add.js
@@ -1,22 +1,22 @@
 const fs = require('fs')
 const path = require('path')
-const getInitFilepath = require('../utils/getInitFilepath')
+const getJsonFilepath = require('../utils/getJsonFilepath')
 
 
-module.exports = ({initFpSeg='init.json', tags, taggingFpSegs}) => {
-	const initFp = getInitFilepath(initFpSeg)
+module.exports = ({tagDirFpSeg='./tag-dir.json', tags, taggingFpSegs}) => {
+	const tagDirFp = getJsonFilepath(tagDirFpSeg)
 
 	const taggingFps = taggingFpSegs.map(taggingFpSeg => path.resolve(process.cwd(),taggingFpSeg))
-	if(fs.existsSync(initFp)) {
-		//TODO: Add code for if the file at initFp isn't properly formatted.
-		const tagDir = JSON.parse(fs.readFileSync(initFp))
+	if(fs.existsSync(tagDirFp)) {
+		//TODO: Add code for if the file at tagDirFp isn't properly formatted.
+		const tagDir = JSON.parse(fs.readFileSync(tagDirFp))
 		tags.forEach( tag => {
 			if(!(tag in tagDir)) tagDir[tag] = []
 			tagDir[tag].push(...taggingFps)
 		})
-		fs.writeFileSync(initFp, JSON.stringify(tagDir))
+		fs.writeFileSync(tagDirFp, JSON.stringify(tagDir))
 		console.log('Finished tagging files.')
 	}
 	else
-		console.log(`${initFp} does not exist.`)
+		console.log(`${tagDirFp} does not exist.`)
 }
