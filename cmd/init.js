6eab716a5376532ba3c5f354d476cb211776517d
TrevorShelby <trvrshelby@gmail.com>
2019-10-27 15:10:30 -0500

    Surrounded init function parameter with an object.

--- cmd/init.js
+++ cmd/init.js
@@ -10,7 +10,7 @@ const getInitFilepath = (initFilepathSegment) => {
 
 
 //initFpSeg is the file path segment given by the user to be used for the init file.
-module.exports = (initFpSeg='./init.json') => {
+module.exports = ({initFpSeg='./init.json'}) => {
 	const initFp = getInitFp(initFpSeg)
 
 	if(fs.existsSync(initFp))
