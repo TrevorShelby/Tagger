c84c0d0cf09b105acc6bd2bba7e472907f0b5f8d
TrevorShelby <trvrshelby@gmail.com>
2019-10-27 15:08:27 -0500

    Created add function that attributes files to tags

---
+++ cmd/add.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const path = require('path')
+
+
+const getInitFilepath = (initFilepathSegment) => {
+	let initFilepath = path.resolve(process.cwd(), initFilepathSegment)
+	if(path.extname(initFilepath) != '.json') initFilepath += '.json'
+	return initFilepath
+}
+
+
+module.exports = ({initFpSeg='init.json', tags, taggingFpSegs}) => {
+	const initFp = getInitFilepath(initFpSeg)
+
+	const taggingFps = taggingFpSegs.map(taggingFpSeg => path.resolve(process.cwd(),taggingFpSeg))
+	if(fs.existsSync(initFp)) {
+		//TODO: Add code for if the file at initFp isn't properly formatted.
+		const tagDir = JSON.parse(fs.readFileSync(initFp))
+		tags.forEach( tag => {
+			if(!(tag in tagDir)) tagDir[tag] = []
+			tagDir[tag].push(...taggingFps)
+		})
+		console.log('Finished tagging files.')
+	}
+	else
+		console.log(`${initFp} does not exist.`)
+}
