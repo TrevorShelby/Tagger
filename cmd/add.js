df5ed81d2f6e25d89b930ece6787025c9ccf2a4a
TrevorShelby <trvrshelby@gmail.com>
2019-10-27 15:19:16 -0500

    Fixed typo so that add and init functions actually write to a file.

--- cmd/add.js
+++ cmd/add.js
@@ -14,7 +14,7 @@ module.exports = ({initFpSeg='init.json', tags, taggingFpSegs}) => {
 			if(!(tag in tagDir)) tagDir[tag] = []
 			tagDir[tag].push(...taggingFps)
 		})
-		fs.write(initFp, JSON.stringify(tagDir))
+		fs.writeFileSync(initFp, JSON.stringify(tagDir))
 		console.log('Finished tagging files.')
 	}
 	else
--- cmd/init.js
+++ cmd/init.js
@@ -4,13 +4,13 @@ const getInitFilepath = require('../utils/getInitFilepath')
 
 
 //initFpSeg is the file path segment given by the user to be used for the init file.
-module.exports = ({initFpSeg='./init.json'}) => {
+module.exports = ({initFpSeg='./init.json'}={}) => {
 	const initFp = getInitFilepath(initFpSeg)
 
 	if(fs.existsSync(initFp))
 		console.log(`${initFp} already exists.`)
 	else {
-		fs.write(initFp, JSON.stringify('{}'))
+		fs.writeFileSync(initFp, JSON.stringify('{}'))
 		console.log(`Initialized ${initFp}.`)
 	}
 }
