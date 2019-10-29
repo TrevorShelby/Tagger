a8fb1dbc4df8420f4ff9770184f473a11a2006e9
TrevorShelby <trvrshelby@gmail.com>
2019-10-29 13:34:07 -0500

    Used more intuitive name for the parameter describing the files to tag.

--- cmd/add.js
+++ cmd/add.js
@@ -3,17 +3,17 @@ const path = require('path')
 const getJsonFilepath = require('../utils/getJsonFilepath')
 
 
-module.exports = ({tagDirFpSeg='./tag-dir.json', tags, taggingFpSegs}) => {
+module.exports = ({tagDirFpSeg='./tag-dir.json', tags, filenames}) => {
 	const tagDirFp = getJsonFilepath(tagDirFpSeg)
 
 	//TODO: Allow for the tagging of directories
-	const taggingFps = taggingFpSegs.map(taggingFpSeg => path.resolve(process.cwd(),taggingFpSeg))
+	const filepaths = filenames.map(filename => path.resolve(process.cwd(),filename))
 	if(fs.existsSync(tagDirFp)) {
 		//TODO: Add code for if the file at tagDirFp isn't properly formatted.
 		const tagDir = JSON.parse(fs.readFileSync(tagDirFp))
 		tags.forEach( tag => {
 			if(!(tag in tagDir)) tagDir[tag] = []
-			tagDir[tag].push(...taggingFps)
+			tagDir[tag].push(...filepaths)
 		})
 		fs.writeFileSync(tagDirFp, JSON.stringify(tagDir))
 		console.log('Finished tagging files.')
