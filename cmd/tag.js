9c6d4f7fe3eea771a6729d9917bfc8c7cf0e1f8e
TrevorShelby <trvrshelby@gmail.com>
2019-10-29 14:35:07 -0500

    Moved logic that reads and write to the tag directory to its own function that tag and untag now use.

--- cmd/tag.js
+++ cmd/tag.js
@@ -1,32 +1,29 @@
 const fs = require('fs')
 const path = require('path')
-const getJsonFilepath = require('../utils/getJsonFilepath')
+const changeTagDir = require('../utils/changeTagDir')
 
 
 module.exports = ({tagDirFilename='tag-dir.json', tags, filenames}) => {
-	const tagDirFilepath = getJsonFilepath(tagDirFilename)
-
 	//TODO: Allow for the tagging of directories
 	const filepaths = filenames.map(filename => {
 		const filepath = path.resolve(process.cwd(),filename)
 		if(fs.existsSync(filepath)) return filepath
 		else console.log(`${filepath} does not exist and will not be tagged.`)
 	}).filter(filepath => filepath != null)
-	if(fs.existsSync(tagDirFilepath)) {
-		//TODO: Add code for if the file at tagDirFilepath isn't properly formatted.
-		const tagDir = JSON.parse(fs.readFileSync(tagDirFilepath))
-		tags.forEach( tag => {
-			if(!(tag in tagDir)) tagDir[tag] = []
-			filepaths.forEach( filepath => {
-				if(tagDir[tag].includes(filepath))
-					console.log(`${filepath} already had the tag ${tag}.`)
-				else
-					tagDir[tag].push(filepath)
+
+	changeTagDir(
+		tagDirFilename,
+		tagDir => {
+			tags.forEach( tag => {
+				if(!(tag in tagDir)) tagDir[tag] = []
+				filepaths.forEach( filepath => {
+					if(tagDir[tag].includes(filepath))
+						console.log(`${filepath} already had the tag ${tag}.`)
+					else
+						tagDir[tag].push(filepath)
+				})
 			})
-		})
-		fs.writeFileSync(tagDirFilepath, JSON.stringify(tagDir))
-		console.log('Finished tagging files.')
-	}
-	else
-		console.log(`${tagDirFilepath} does not exist.`)
+		},
+		() => console.log('Finished tagging files.')
+	)
 }
---
+++ utils/changeTagDir.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const getJsonFilepath = require('./getJsonFilepath')
+
+
+module.exports = (tagDirFilename, makeChange, onWrite) => {
+	const tagDirFilepath = getJsonFilepath(tagDirFilename)
+	if(fs.existsSync(tagDirFilepath)) {
+		//TODO: Add code for if the file at tagDirFilepath isn't properly formatted.
+		const tagDir = JSON.parse(fs.readFileSync(tagDirFilepath))
+		makeChange(tagDir)
+		fs.writeFileSync(tagDirFilepath, JSON.stringify(tagDir))
+		onWrite(tagDir)
+	}
+	else
+		console.log(`${tagDirFilepath} does not exist.`)
+}
