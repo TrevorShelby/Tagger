93b97994297d154e2b2cfbd9dd3f13b9230c439a
TrevorShelby <trvrshelby@gmail.com>
2019-10-29 14:37:26 -0500

    Changed index.js into a CLI tool.

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-module.exports = {
-	init: (args) => {},
-	add: (args) => {},
-	remove: (args) => {}
+const parseArgs = require('minimist')
+
+const initTagDir = require('./cmd/init')
+const addTags = require('./cmd/tag')
+const removeTags = require('./cmd/untag')
+
+
+module.exports = () => {
+	const args = parseArgs(process.argv.slice(2))
+	const cmd = args._[0]
+
+	if(cmd == 'init')
+		initTagDir({tagDirFilename: args._[1] || 'tag-dir.json'})
+	else if(cmd == 'tag')
+		addTags({
+			tagDirFilename: args._[1] || 'tag-dir.json',
+			tags: (args.tags || args.t).split(','),
+			filenames: (args.files || args.f).split(',')
+		})
+	else if(cmd == 'untag')
+		removeTags({
+			tagDirFilename: args._[1] || 'tag-dir.json',
+			tags: (args.tag || args.t).split(','),
+			filenames: (args.files || args.f).split(',')
+		})
 }
