75f1a1a352a1fac3bf5e6d08cd5960c39ef71df1
TrevorShelby <trvrshelby@gmail.com>
2019-10-29 13:45:07 -0500

    add command now checks if the filepaths given for tagging really exist.

--- cmd/add.js
+++ cmd/add.js
@@ -7,7 +7,11 @@ module.exports = ({tagDirFilename='tag-dir.json', tags, filenames}) => {
 	const tagDirFilepath = getJsonFilepath(tagDirFilename)
 
 	//TODO: Allow for the tagging of directories
-	const filepaths = filenames.map(filename => path.resolve(process.cwd(),filename))
+	const filepaths = filenames.map(filename => {
+		const filepath = path.resolve(process.cwd(),filename)
+		if(fs.existsSync(filepath)) return filepath
+		else console.log(`${filepath} does not exist and will not be tagged.`)
+	})
 	if(fs.existsSync(tagDirFilepath)) {
 		//TODO: Add code for if the file at tagDirFilepath isn't properly formatted.
 		const tagDir = JSON.parse(fs.readFileSync(tagDirFilepath))
