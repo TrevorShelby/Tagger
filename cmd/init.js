1ab93da5902d82ebd3c5d970f0d19b655cc9c14b
TrevorShelby <trvrshelby@gmail.com>
2019-10-30 09:34:54 -0500

    if the tag directory specified by the given path already exists, the init command will now handle the warning as an error and quit.

--- cmd/init.js
+++ cmd/init.js
@@ -5,8 +5,10 @@ const getJsonFilepath = require('../utils/getJsonFilepath')
 module.exports = ({tagDirFilename='tag-dir.json'}={}) => {
 	const tagDirFilepath = getJsonFilepath(tagDirFilename)
 
-	if(fs.existsSync(tagDirFilepath))
-		console.log(`${tagDirFilepath} already exists.`)
+	if(fs.existsSync(tagDirFilepath)) {
+		console.error(`${tagDirFilepath} already exists.`)
+		process.exit(1)
+	}
 	else {
 		fs.writeFileSync(tagDirFilepath, JSON.stringify({}))
 		console.log(`Initialized ${tagDirFilepath}.`)
