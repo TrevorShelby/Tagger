a74d0326c9e16d979b8b23756cb49717469e2858
TrevorShelby <trvrshelby@gmail.com>
2019-11-03 08:03:46 -0600

    Removed unadded commands.

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const parseArgs = require('minimist')
 
 const initTagDir = require('./cmd/init')
-const tagFiles = require('./cmd/tag')
-const untagFiles = require('./cmd/untag')
-const removeFiles = require('./cmd/removeFiles')
-const removeTags = require('./cmd/removeTags')
-const search = require('./cmd/search')
-
 
 module.exports = () => {
 	const args = parseArgs(process.argv.slice(2))
@@ -14,37 +8,6 @@ module.exports = () => {
 
 	if(cmd == 'i' || cmd == 'init' || cmd == 'initialize')
 		initTagDir({tagDirFilename: args._[1] || 'tag-dir.json'})
-	else if(cmd == 't' || cmd == 'tag')
-		tagFiles({
-			tagDirFilename: args._[1] || 'tag-dir.json',
-			tags: (args.tags || args.t).split(','),
-			filenames: (args.files || args.f).split(',')
-		})
-	else if(cmd == 'un' || cmd == 'untag')
-		untagFiles({
-			tagDirFilename: args._[1] || 'tag-dir.json',
-			tags: (args.tag || args.t).split(','),
-			filenames: (args.files || args.f).split(',')
-		})
-	else if(cmd == 'rf' || cmd == 'remove-files')
-		//TODO: Try to figure out a way where filenames can be non-option arguments while still
-		//accounting for the optional tagDirFilename argument.
-		removeFiles({
-			tagDirFilename: args._[1] || 'tag-dir.json',
-			filenames: (args.files || args.f).split(',')
-		})
-	else if(cmd == 'rt' || cmd == 'remove-tags')
-		removeTags({
-			tagDirFilename: args._[1] || 'tag-dir.json',
-			tags: (args.tag || args.t).split(',')
-		})
-	else if(cmd == 'search')
-		search({
-			tagDirFilename: args._[1] || 'tag-dir.json',
-			query: args.q || args.query
-		})
 	else
 		console.log('Unrecognized command.')
-	//TODO: Create a rename command to rename all instances of a path in the tag directory.
-	//TODO: Add help and version command.
 }
