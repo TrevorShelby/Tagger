a3a237955f72b148968ec481ca9cda48d382d215
TrevorShelby <trvrshelby@gmail.com>
2019-11-04 14:10:04 -0600

    Made the tags option optional, passing an empty array when it is not present.

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const newLink = require('./cmd/newLink.js')
 
 
 const getDirName = args => args.directory || args.d || 'tag-dir.json'
+const getTags = args => {
+	const csvTags = args.tags || args.t
+	if(csvTags)
+		return (csvTags).split(',')
+	else
+		return []
+}
 
 module.exports = () => {
 	const args = parseArgs(process.argv.slice(2))
@@ -12,7 +19,7 @@ module.exports = () => {
 	if(cmd == 'i' || cmd == 'init' || cmd == 'initialize')
 		initTagDir(getDirName(args))
 	else if(cmd == 'newLink' || cmd == 'new' || cmd == 'link' || cmd == 'l')
-		newLink(getDirName(args), args.source || args.s, args._[1])
+		newLink(getDirName(args), args.source || args.s, args._[1], getTags(args))
 	else
 		console.log('Unrecognized command.')
 }
