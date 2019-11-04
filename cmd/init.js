a45d13641ed25291147ef3ad83d1b805a34aa5c1
TrevorShelby <trvrshelby@gmail.com>
2019-11-04 14:28:05 -0600

    Fixed bug when display filepath in init, as well as another bug that output the tag directory file as a string.

--- cmd/init.js
+++ cmd/init.js
@@ -5,9 +5,9 @@ const handleIssue = require('../utils/handleIssue')
 module.exports = (tagDirFilename='tag-dir.json') => {
 	tagDirFile = new TagDirectoryFile(tagDirFilename)
 	if(tagDirFile.exists())
-		handleIssue('error', '000', tagDirFilepath)
+		handleIssue('error', '000', tagDirFile.filepath)
 	else {
-		tagDirFile.write(JSON.stringify( {links: {}, tags: {}} ))
-		handleIssue('info', '100', tagDirFilepath)
+		tagDirFile.write({links: {}, tags: {}})
+		handleIssue('info', '100', tagDirFile.filepath)
 	}
 }
