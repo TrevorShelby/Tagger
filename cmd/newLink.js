929e11b6152356c4ffff65991749e9261dea21c5
TrevorShelby <trvrshelby@gmail.com>
2019-11-05 13:07:05 -0600

    Separated the newLink command from the code that decides how to get access a link's location given an arbitrary source.

--- cmd/newLink.js
+++ cmd/newLink.js
@@ -6,15 +6,6 @@ const TagDirectoryFile = require('../utils/tagDirectoryFile')
 const handleIssue = require('../utils/handleIssue')
 
 
-//TODO: Create source-based functions that check for locations with repeat listings. e.g. for
-//local-fs, a function that checks to see if there are any other links that use the same filepath
-//for their location.
-const getLocalFsPath = filename => {
-	const filepath = path.resolve(process.cwd(), filename)
-	if(!fs.existsSync(filepath)) return
-	else return filepath
-}
-
 const reportNewLinkIssues = val => {
 	if(val.done) handleIssue('info', '101')
 	else {
@@ -23,30 +14,71 @@ const reportNewLinkIssues = val => {
 	}
 }
 
-const reportTagDirIssues = val => {
+const reportTagDirIssues_ = (links, getDisplayName) => val => {
 	if(!val.done) {
 		//TODO: Consider removing process.exit(1) from handleIssues, so that multiple errors can be
 		//listed.
-		//TODO: Replace val.lid with a function that prints out an appropriate display for the
-		//links location, given its source (which should be partially applied to this function).
-		if(val.event == 'linkMissingTag') handleIssue('error', '012', val.lid, val.tag)
-		else if(val.event == 'tagMissingLink') handleIssue('error', '013', val.lid, val.tag)
+		const location = links[val.lid].location
+		if(val.event == 'linkMissingTag')
+			handleIssue('error', '012', getDisplayName(location), val.tag)
+		else if(val.event == 'tagMissingLink')
+			handleIssue('error', '013', getDisplayName(location), val.tag)
 		val.promise.then(reportTagDirIssues)
 	}
 }
 
-module.exports = (tagDirFilepath='tag-dir.json', source, arg, tags) => {
-	const location = (() => {
-		if(source == 'local-fs') return getLocalFsPath(arg)
-	})()
-	if(location == undefined) throw Error('undefined location')
+const sourceScripts = {
+	'local-fs': {
+		cliInputToLocation: filename => path.resolve(process.cwd(), filename),
+		getLinkInfo: (location, fields) => {
+			const linkInfo = {}
+			fields.forEach( field => {
+				if(field == 'exists')
+					linkInfo.exists = fs.existsSync(location)
+				else if(field == 'equals')
+					linkInfo.equals = otherLocation => location == otherLocation
+				else if(field == 'displayName')
+					linkInfo.displayName = location
+			})
+			return linkInfo
+		}
+	}
+}
+
+
+module.exports = async (tagDirFilepath='tag-dir.json', source, arg, tags) => {
+	//TODO: Figure something out if sourceScripts doesn't have the listed source.
+	const { cliInputToLocation, getLinkInfo } = sourceScripts[source]
+	const location = cliInputToLocation(arg)
+	const {
+		exists: linkExists,
+		equals: linkEquals,
+		displayName
+	} = getLinkInfo(location, ['exists', 'equals', 'displayName'])
+
+	//TODO: Handle error more effectively.
+	if(!linkExists)
+		throw new Error('location doesn\'t exist.')
+
 	const tagDirFile = new TagDirectoryFile(tagDirFilepath)
 	if(!tagDirFile.exists())
 		handleIssue('error', '001', tagDirFilepath)
 	else {
 		const tagDir = new TagDirectory(tagDirFile.read())
-		tagDir.validate().then(reportTagDirIssues)
-		tagDir.newLink({source, location, tags}).then(reportNewLinkIssues)
+
+		//Checks for a duplicate link.
+		const lids = Object.keys(tagDir.links)
+		const duplicateLinkLid = lids
+			.filter( lid => tagDir.links[lid].source == source )
+			.find( lid => linkEquals(tagDir.links[lid].location) )
+		if(duplicateLinkLid != undefined)
+			handleIssue('error', '019', displayName, duplicateLinkLid)
+
+		await tagDir.validate().then(reportTagDirIssues_(
+			tagDir.links,
+			location => getLinkInfo(location, ['displayName']).displayName
+		))
+		await tagDir.newLink({source, location, tags}).then(reportNewLinkIssues)
 		tagDirFile.write(tagDir)
 	}
 }
