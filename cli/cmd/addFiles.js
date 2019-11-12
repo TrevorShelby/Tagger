ae96b4908cdda867e5de4cc74f880f5e232444ce
TrevorShelby <trvrshelby@gmail.com>
2019-11-12 17:04:03 -0600

    Added new tagDirIo to addFiles.

--- cli/cmd/addFiles.js
+++ cli/cmd/addFiles.js
@@ -1,13 +1,11 @@
 const path = require('path')
-const getJsonFilepath = require('../../utils/getJsonFilepath')
-const { readTagDirectory, writeTagDirectory } = require('../../utils/tagDirIo')
+const { readTagDirectory, writeTagDirectory } = require('../tagDirIo')
 const notify = require('../tagDirNotifications')
 
 
 module.exports = ({tagDirFilename='tag-dir.json', filenames}) => {
-	const tagDirFilepath = getJsonFilepath(tagDirFilename)
-	const tagDir = readTagDirectory(tagDirFilepath)
+	const tagDir = readTagDirectory(tagDirFilename)
 	const filepaths = filenames.map(filename => path.resolve(process.cwd(), filename))
 	tagDir.addFiles(filepaths, notify)
-	writeTagDirectory(tagDirFilepath, tagDir)
+	writeTagDirectory(tagDirFilename, tagDir)
 }
