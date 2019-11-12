de72ace9967b4accca1744932564f4b08f151d13
TrevorShelby <trvrshelby@gmail.com>
2019-11-12 17:03:37 -0600

    Added tagDirIo for CLI that lets CLI user write just the filename.

---
+++ cli/tagDirIo.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const { readTagDirectory, writeTagDirectory } = require('../../utils/tagDirIo')
+
+
+
+const getJsonFilepath = filename => {
+	let jsonFilepath = path.resolve(process.cwd(), filename)
+	if(path.extname(jsonFilepath) != '.json') jsonFilepath += '.json'
+	return jsonFilepath
+}
+
+
+const cliReadTagDirectory = tagDirFilename => {
+	const tagDirFilepath = getJsonFilepath(tagDirFilename)
+	return readTagDirectory(tagDirFilepath)
+}
+
+const cliWriteTagDirectory = (tagDirFilename, tagDir) => {
+	const tagDirFilepath = getJsonFilepath(tagDirFilename)
+	writeTagDirectory(tagDirFilepath, tagDir)
+}
+
+
+
+module.exports = {
+	readTagDirectory: cliReadTagDirectory,
+	writeTagDirectory: cliWriteTagDirectory
+}
