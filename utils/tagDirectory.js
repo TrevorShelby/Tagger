1570016325bf46698afa2854896470dc097839c1
TrevorShelby <trvrshelby@gmail.com>
2019-11-12 14:41:31 -0600

    Added functionality to add files to TagDirectory, as well as fleshing out the event-based stuffs with TagDirectory.

--- utils/tagDirectory.js
+++ utils/tagDirectory.js
@@ -1,9 +1,36 @@
-const Issue = {
+const NotificationType = {
+	ERROR: 0,
+	WARNING: 1,
+	INFO: 2
+}
+
+const Notification = {
+	FILE_ALREADY_EXISTS: 0,
 	FILE_ALREADY_TAGGED: 1,
 	NONEXISTENT_FILE: 2,
 	NONEXISTENT_TAG: 3,
 	TAG_NOT_IN_FILE: 4,
-	FILE_NOT_IN_TAG: 5
+	FILE_NOT_IN_TAG: 5,
+	ADDED_FILE: 6,
+	TAGGED_FILE: 7,
+	UNTAGGED_FILE: 8,
+	REMOVED_FILE: 9,
+	ADDED_TAG: 10,
+	REMOVED_TAG: 11
+}
+Notification.types = {
+	[Notification.FILE_ALREADY_EXISTS]: NotificationType.WARNING,
+	[Notification.FILE_ALREADY_TAGGED]: NotificationType.WARNING,
+	[Notification.NONEXISTENT_FILE]: NotificationType.WARNING,
+	[Notification.NONEXISTENT_TAG]: NotificationType.WARNING,
+	[Notification.TAG_NOT_IN_FILE]: NotificationType.WARNING,
+	[Notification.FILE_NOT_IN_TAG]: NotificationType.ERROR,
+	[Notification.ADDED_FILE]: NotificationType.INFO,
+	[Notification.TAGGED_FILE]: NotificationType.INFO,
+	[Notification.UNTAGGED_FILE]: NotificationType.INFO,
+	[Notification.REMOVED_FILE]: NotificationType.INFO,
+	[Notification.ADDED_TAG]: NotificationType.INFO,
+	[Notification.REMOVED_TAG]: NotificationType.INFO
 }
 
 
@@ -14,50 +41,65 @@ const TagDirectory = class {
 		this.tags = tags
 	}
 
-	tagFiles(files, tags, cb=()=>{}) {
-		if(tags.length == 0)
-			return
+	addFiles(files, cb=()=>{}) {
 		files.forEach( file => {
-			if(!(file in this.files))
-				this.files[file] = []
-			tags.forEach( tag => {
-				if(!(tag in this.tags))
-					this.tags[tag] = []
-				if(this.tags[tag].includes(file)) {
-					cb(Issue.FILE_ALREADY_TAGGED, file, tag)
-					return
-				}
-				else {
-					this.files[file].push(tag)
-					this.tags[tag].push(file)
-				}
-			})
+			if(this.files.includes(file)) {
+				cb(Notification.FILE_ALREADY_EXISTS, file)
+				return
+			}
+			this.files[file] = {}
+			cb(Notification.ADDED_FILE, file)
 		})
 	}
 
-	untagFiles(files, tags, cb=()=>{}) {
+	tagFiles(files, tags, cb=()=>{}) {
 		files.forEach( file => {
 			if(!(file in this.files)) {
-				cb(Issue.NONEXISTENT_FILE, file)
+				cb(Notification.NONEXISTENT_FILE, file)
 				return
 			}
 			tags.forEach( tag => {
 				if(!(tag in this.tags)) {
-					cb(Issue.NONEXISTENT_TAG, tag)
+					this.tags[tag] = []
+					cb(Notification.CREATED_TAG, tag)
+				}
+				if(this.tags[tag].includes(file)) {
+					cb(Notification.FILE_ALREADY_TAGGED, file, tag)
+					return
+				}
+				file.push(tag)
+				tag.push(file)
+				cb(Notification.TAGGED_FILE, file, tag)
+			})
+		})
+	}
+
+	//it would be appropriate for cb to throw an error if the TAG_NOT_IN_FILE notification is
+	//applied to it.
+	untagFiles(files, tags, cb=()=>{}) {
+		files.forEach( file => {
+			if(!(file in this.files)) {
+				cb(Notification.NONEXISTENT_FILE, file)
+				return
+			}
+			tags.forEach( tag => {
+				if(!(tag in this.tags)) {
+					cb(Notification.NONEXISTENT_TAG, tag)
 					return
 				}
 				const tagIndex = this.files[file].indexOf(tag)
 				if(tagIndex == -1) {
-					cb(Issue.TAG_NOT_IN_FILE, file, tag)
+					cb(Notification.TAG_NOT_IN_FILE, file, tag)
 					return
 				}
 				const fileIndex = this.tags[tag].indexOf(file)
 				if(fileIndex == -1) {
-					cb(Issue.FILE_NOT_IN_TAG, file, tag)
+					cb(Notification.FILE_NOT_IN_TAG, file, tag)
 					return
 				}
 				this.files[file].splice(tagIndex, 1)
 				this.tags[tag].splice(fileIndex, 1)
+				cb(Notification.UNTAGGED_FILE, file, tag)
 			})
 		})
 	}
@@ -65,21 +107,23 @@ const TagDirectory = class {
 	removeTags(tags, cb=()=>{}) {
 		tags.forEach( tag => {
 			if(!(tag in this.tags)) {
-				cb(Issue.NONEXISTENT_TAG, tag)
+				cb(Notification.NONEXISTENT_TAG, tag)
 				return
 			}
 			this.tags[tag].forEach( taggedFile => {
 				const tagIndex = this.files[taggedFile].indexOf(tag)
 				this.files[taggedFile].splice(tagIndex, 1)
+				cb(Notification.UNTAGGED_FILE, taggedFile, tag)
 			})
 			delete this.tags[tag]
+			cb(Notification.REMOVED_TAG, tag)
 		})
 	}
 
 	removeFiles(files, cb=()=>{}) {
 		files.forEach( file => {
 			if(!(file in this.files)) {
-				cb(Issue.NONEXISTENT_FILE, file)
+				cb(Notification.NONEXISTENT_FILE, file)
 				return
 			}
 			this.files[file].forEach( tagWithFile => {
@@ -87,10 +131,11 @@ const TagDirectory = class {
 				this.tags[tagWithFile].splice(fileIndex, 1)
 			})
 			delete this.files[file]
+			cb(Notification.REMOVED_FILE, file)
 		})
 	}
 }
 
 
 
-module.exports = { Issue, TagDirectory }
+module.exports = { Notification, NotificationType, TagDirectory }
