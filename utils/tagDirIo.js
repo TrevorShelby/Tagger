95a663a66df213b9742198251f2ff299e7e5110a
TrevorShelby <trvrshelby@gmail.com>
2019-11-12 16:10:45 -0600

    Added TODO

--- utils/tagDirIo.js
+++ utils/tagDirIo.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const TagDirectory = require('./tagDirectory').TagDirectory
 
 
+//TODO: Add CLI implementation that handles errors.
 
 const readTagDirectory = tagDirFilepath => {
 	const tagDirJson = JSON.parse(fs.readFileSync(tagDirFilepath, {encoding: 'utf-8'}))
