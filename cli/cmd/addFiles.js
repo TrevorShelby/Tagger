5a3dffa0f4e8fc826a08dc2d997c9cfdee1ba137
TrevorShelby <trvrshelby@gmail.com>
2019-11-14 09:55:38 -0600

    Only files that exist will be added to the directory now.

--- cli/cmd/addFiles.js
+++ cli/cmd/addFiles.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const path = require('path')
 const { readTagDirectory, writeTagDirectory } = require('../tagDirIo')
 const notify = require('../tagDirNotifications')
@@ -5,7 +6,17 @@ const notify = require('../tagDirNotifications')
 
 module.exports = ({tagDirFilename='tag-dir.json', filenames}) => {
 	const tagDir = readTagDirectory(tagDirFilename)
-	const filepaths = filenames.map(filename => path.resolve(process.cwd(), filename))
-	tagDir.addFiles(filepaths, notify)
+	const filepaths = filenames
+		.map(filename => path.resolve(process.cwd(), filename))
+		.reduce((filepaths, filepath) => {
+			if(fs.existsSync(filepath))
+				filepaths.existent.push(filepath)
+			else
+				filepaths.nonexistent.push(filepath)
+			return filepaths
+		}, {existent: [], nonexistent: []})
+	filepaths.nonexistent
+		.forEach( filepath => console.log(`${filepath} does not exist and won't be added to this directory.`) )
+	tagDir.addFiles(filepaths.existent, notify)
 	writeTagDirectory(tagDirFilename, tagDir)
 }
